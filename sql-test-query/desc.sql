a6b77621fdab6c5acd561af7dcda1b1d2afd4a46
thejae-u <31125543+thejae-u@users.noreply.github.com>
2025-04-04 01:56:11 +0900

    refact: Rider IDE 변경, ReadMe 수정, gitignore 수정

---
+++ sql-test-query/desc.sql
@@ -0,0 +1,6 @@
+use mmo_server_data;
+
+-- select * from server_log;
+-- truncate table server_log;
+
+desc user_log;
---
+++ sql-test-query/select.sql
@@ -0,0 +1,5 @@
+use mmo_server_data;
+
+-- select uuid from users where user_name = 'alice';
+select * from user_log;
+select * from server_log;
